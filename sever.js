e8eb7af4457433aa4711629f2ec2804ec9fc0517
tuanfec <132864211+tuanfec@users.noreply.github.com>
2024-09-24 15:02:15 +0700

    thiết lập server

---
+++ sever.js
@@ -0,0 +1,58 @@
+import express, { json } from "express";
+import dotnv from "dotenv";
+import cors from "cors";
+import multer from "multer";
+import createError from "http-errors";
+import path from "path";
+import connectDB from "./database.js";
+import http from "http";
+import adminRouter from "./routes/index.js";
+
+const app = express();
+dotnv.config();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(json());
+
+const port = process.env.PORT || 9999;
+const server = http.createServer(app);
+const io = setupSocket(server);
+
+app.set("io", io);
+
+// Cấu hình multer và các route
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+app.use("/admins", adminRouter);
+
+app.use(async (req, res, next) => {
+  next(createError.NotFound());
+});
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: err.status || 500,
+    message: err.message,
+  });
+});
+
+server.listen(port, () => {
+  connectDB();
+  console.log(`listening on ${port}`);
+});
